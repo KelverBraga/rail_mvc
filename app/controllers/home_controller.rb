661f5a95acf82462adeab296e6c409055c5a1276
KelverBraga <kelverruanbraga@gmail.comgit config --global user.email kelverruanbraga@gmail.comgit config --global user.email kelverruanbraga@gmail.com>
2026-02-21 17:16:31 -0300

    db

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @teste = "My Journey into Software Development
-              <hr />
-              I am currently learning Ruby on Rails and improving my backend development skills. Every day, I practice building small projects, creating controllers, models, and routes. At the beginning, I faced many errors, but each mistake helped me understand the framework better.
-              My background is in engineering, where I worked with automation systems and industrial projects. Now, I am expanding my knowledge into software development because technology is constantly evolving, and I want to grow professionally.
-              My goal is to work remotely for an international company, contribute to meaningful projects, and continuously improve my English communication skills. I believe that consistency, discipline, and practice are the keys to success."
+    @teste = "Hello, World!"
   end 
 end
---
+++ db/migrate/20260221200028_create_cars.rb
@@ -0,0 +1,12 @@
+class CreateCars < ActiveRecord::Migration[8.1]
+  def change
+    create_table :cars do |t|
+      t.string :model
+      t.integer :year 
+      t.string :color
+      t.boolean :electric
+      t.boolean :is_selling
+      t.timestamps   
+    end
+  end
+end
